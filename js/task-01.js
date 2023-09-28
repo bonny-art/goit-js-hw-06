087bbe7383fc2c4eb1e77d82c250303021ec15eb
bonny-art <svitlana.otenko@gmail.com>
2023-09-28 16:56:17 +0300

    fixes bugs

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,10 @@
 const menuEl = document.querySelector('#categories');
 
-console.log(`Number of categories: ${menuEl.children.length}`);
+console.log(typeof menuEl.children.length);
+
+console.log('Number of categories:', menuEl.children.length);
 
 [...menuEl.children].forEach(category => {
-  console.log(`Category: ${category.firstElementChild.textContent}`);
-  console.log(`Elements: ${category.lastElementChild.children.length}`);
+  console.log('Category:', category.firstElementChild.textContent);
+  console.log('Elements:', category.lastElementChild.children.length);
 });
